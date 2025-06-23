9bd05177c43531ddc6687edff0542e7d1e6c7e40
geralnb <shirometeora@gmail.com>
2025-06-23 15:20:36 +0700

    refactor infota ke TAKU

--- resources/views/admin/profil.blade.php
+++ resources/views/admin/profil.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>InfoTA - Profil</title>
+    <title>Profil - TAKU</title>
     <link rel="stylesheet" href="{{ asset('/storage/assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
@@ -288,7 +288,7 @@
             </div>
             <footer class="bg-white sticky-footer">
                 <div class="container my-auto">
-                    <div class="text-center my-auto copyright"><span>Copyright © infoTA 2025</span></div>
+                    <div class="text-center my-auto copyright"><span>Copyright © TAKU 2025</span></div>
                 </div>
             </footer>
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
